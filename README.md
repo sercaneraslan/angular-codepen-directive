a8c674ea8fa39cb1f9a1437833e57d2b4286c664
Sercan Eraslan <sercan.eraslan@sahibinden.com>
2014-10-28 18:27:07 +0200

    ng-codepen added

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 angular-codepen-directive
 =========================
 
-Angular Codepen Directive
+Angular <a href="http://codepen.io/">Codepen.io</a> Directive
+
+Usage:
+
+```
+<div codepen cp-user="sercaneraslan" cp-slug-hash="sFJzC" cp-height="330" cp-theme-id="9501" cp-default-tab="result"></div>
+```
---
+++ ng-codepen.js
@@ -0,0 +1,38 @@
+/*
+*
+* ng-codepen v0.0.1
+*
+* (c) 2013-2014 Sercan Eraslan http://sercaneraslan.com
+* License: MIT
+*
+*/
+angular.module('ngCodepen', []).
+    directive('codepen', [function() {
+
+        return {
+            restrict: 'A',
+            replace: true,
+            transclude: true,
+            scope: true,
+            template: '<iframe scrolling="no" frameborder="0" class="cp_embed_iframe" allowtransparency="true" allowfullscreen="true" style="width: 100%; overflow: hidden;"></iframe>',
+            controller: function ($scope, $element, $attrs, $transclude) {
+
+                var url = document.location.protocol
+                    + '//codepen.io/' + $attrs.cpUser
+                    + '/embed/' + $attrs.cpSlugHash
+                    + '?user=' + $attrs.cpUser
+                    + '&height=' + $attrs.cpHeight
+                    + '&theme-id=' + $attrs.cpThemeId
+                    + '&slug-hash=' + $attrs.cpSlugHash
+                    + '&default-tab=' + $attrs.cpDefaultTab;
+
+                $transclude(function () {
+                    $element.attr('src', url)
+                        .attr('height', $attrs.cpHeight)
+                        .attr('id', 'cp_embed_' + $attrs.cpSlugHash);
+                });
+            }
+        };
+    }]
+);
+
